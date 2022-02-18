1b5df0bd38c7a20ab8ec8efec45774b1ee76e1e3
teknolandroid <android@teknol.xyz>
2022-02-18 20:54:38 +0530

    back button bug resolved

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 // import '../screens/products_overview_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -14,7 +15,10 @@ class DashboardScreen extends StatefulWidget {
   _DashboardScreenState createState() => _DashboardScreenState();
 }
 
+late WebViewController controllerGlobal;
+
 class _DashboardScreenState extends State<DashboardScreen> {
+  late WebViewController _controller1;
   final Completer<WebViewController> _controller =
       Completer<WebViewController>();
   final CookieManager cookieManager = CookieManager();
@@ -30,53 +34,90 @@ class _DashboardScreenState extends State<DashboardScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Builder(builder: (BuildContext context) {
-        return WebView(
-          initialUrl: '$urls',
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (WebViewController webViewController) {
-            _controller.complete(webViewController);
-          },
-          onProgress: (int progress) {
-            // _showDialogError('TEST', 'Test1');
-            print("WebView is loading (progress : $progress%)");
-          },
-          javascriptChannels: <JavascriptChannel>{
-            _toasterJavascriptChannel(context),
-          },
-          navigationDelegate: (NavigationRequest request) {
-            if (request.url.toString().contains('customer/account/logout')) {
-              removeSharedPref();
+    return WillPopScope(
+      onWillPop: () => _goBack(context),
+      child: Scaffold(
+        // We're using a Builder here so we have a context that is below the Scaffold
+        // to allow calling Scaffold.of(context) so we can show a snackbar.
+        body: Builder(builder: (BuildContext context) {
+          return WebView(
+            initialUrl: '$urls',
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController webViewController) {
+              _controller.future.then((value) => _controller1 = value);
+              _controller.complete(webViewController);
+            },
+            onProgress: (int progress) {
+              // _showDialogError('TEST', 'Test1');
+              print("WebView is loading (progress : $progress%)");
+            },
+            javascriptChannels: <JavascriptChannel>{
+              _toasterJavascriptChannel(context),
+            },
+            navigationDelegate: (NavigationRequest request) {
+              if (request.url.toString().contains('customer/account/logout')) {
+                removeSharedPref();
 
-              _onClearCookies(context);
-              print('blocking navigation to $request}');
-              Navigator.of(context)
-                  .pushReplacementNamed(AuthScreen.routeName, arguments: "");
-              return NavigationDecision.prevent;
-            }
-            if (request.url.toString().contains('customer/account/login')) {
-              removeSharedPref();
-              _onClearCookies(context);
-              print('blocking navigation to $request}');
-              Navigator.of(context)
-                  .pushReplacementNamed(AuthScreen.routeName, arguments: "");
-              return NavigationDecision.prevent;
-            }
-            print('allowing navigation to $request');
-            return NavigationDecision.navigate;
-          },
-          onPageStarted: (String url) {
-            print('Page started loading: $url');
-          },
-          onPageFinished: (String url) {
-            print('Page finished loading: $url');
-          },
-          gestureNavigationEnabled: true,
-        );
-      }),
-      // floatingActionButton: favoriteButton(),
+                _onClearCookies(context);
+                print('blocking navigation to $request}');
+                Navigator.of(context)
+                    .pushReplacementNamed(AuthScreen.routeName, arguments: "");
+                return NavigationDecision.prevent;
+              }
+              if (request.url.toString().contains('customer/account/login')) {
+                removeSharedPref();
+                _onClearCookies(context);
+                print('blocking navigation to $request}');
+                Navigator.of(context)
+                    .pushReplacementNamed(AuthScreen.routeName, arguments: "");
+                return NavigationDecision.prevent;
+              }
+              print('allowing navigation to $request');
+              return NavigationDecision.navigate;
+            },
+            onPageStarted: (String url) {
+              print('Page started loading: $url');
+            },
+            onPageFinished: (String url) {
+              print('Page finished loading: $url');
+            },
+            gestureNavigationEnabled: true,
+          );
+        }),
+      ),
     );
+    // return Scaffold(
+
+    //   // floatingActionButton: favoriteButton(),
+    // );
+  }
+
+  Future<bool> _goBack(BuildContext context) async {
+    if (await _controller1.canGoBack()) {
+      _controller1.goBack();
+      return Future.value(false);
+    } else {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: Text('Do you want to exit'),
+                actions: <Widget>[
+                  FlatButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('No'),
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      SystemNavigator.pop();
+                    },
+                    child: Text('Yes'),
+                  ),
+                ],
+              ));
+      return Future.value(true);
+    }
   }
 
   Future<String> getSharedPref() async {
